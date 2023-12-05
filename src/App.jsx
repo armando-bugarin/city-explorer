69b8f2df532dd5666a2db5081287975b4bbf9157
ARMANDO BUGARIN <armando.bugarin24@gmail.com>
2023-12-04 22:29:42 -0800

    make search form; lab6

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Alert } from 'bootstrap';
+import './App.css';
+
+const key = import.meta.env.VITE_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [location, setLocation] = useState({ display_name: 'info' });
+  const [findCity, setFindCity] = useState('');
+  const [error, setError] = useState(null);
+
+  async function getLocation() {
+    try {
+      const API = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${findCity}&format=json`;
+      const response = await axios.get(API);
+      const results = response.data[0];
+      setLocation(results);
+      setError(null);
+    } catch (error) {
+      setError('An error occurred with the API call');
+    }
+  }
+  function onSearchChange(event) {
+    setFindCity(event.target.value);
+  }
+  function searchLocation(event) {
+    event.preventDefault();
+    getLocation();
+    setFindCity({ display_name: findCity });
+  }
+  function generateMap(lat, lon) {
+    const API2 = `https://maps.locationiq.com/v3/staticmap?key=${key}&center=${lat},${lon}&zoom=10`;
+    return API2;
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input onChange={onSearchChange} />
+      <button onClick={searchLocation}>Explore City</button>
+      <h2>City Name: {location.display_name}</h2>
+      <h2>City Latitude: {location.lat}</h2> <h2>City Longitude: {location.lon}</h2>
+      {error && (
+        <Alert variant="danger" onClose={() => setError(null)} dismissible>
+          {error}
+        </Alert>
+      )}
+      {location.lat && location.lon && <img src={generateMap(location.lat, location.lon)}
+        alt="map"
+        style={{ maxWidth: '80%' }}
+      />}
     </>
   )
 }
 
-export default App
+export default App;
